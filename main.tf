30043f05bea8fb13a2d18bedacd43a106f685403
priyankdeshmukh <priyankdeshmukh300@gmail.com>
2022-08-04 09:46:05 +0000

    second push

--- main.tf
+++ main.tf
@@ -8,11 +8,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIAR7M2RYXVIO7CFTNR"
-  secret_key = "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"
-}
 
 resource "aws_instance" "test_instance" {
   ami           = "ami-052efd3df9dad4825"
