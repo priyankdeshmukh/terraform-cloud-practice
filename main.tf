2cfc9e5cbc63e40164e3f8be5b0911e430197688
priyankdeshmukh <priyankdeshmukh300@gmail.com>
2022-08-04 09:50:19 +0000

    third push

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.24.0"
-    }
-  }
-}
-
-
 resource "aws_instance" "test_instance" {
   ami           = "ami-052efd3df9dad4825"
   instance_type = "t3.micro"
